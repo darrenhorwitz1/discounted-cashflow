61e019fa9e1bc6a736af270d4472ad1f87750c38
darrenhorwitz <darrenhorwitz1@gmail.com>
2021-03-04 20:05:50 +0200

    updated expense applyForecast

--- src/line-item/line-items.ts/expense.ts
+++ src/line-item/line-items.ts/expense.ts
@@ -14,17 +14,31 @@ class Expense implements LineItem, TaxableLineItem {
     return this.type;
   }
   applyForecast(
-    input?: Input,
+    input: Input,
     previousCashflow?: Cashflow,
     currentCashflow?: Cashflow
   ): void {
     //do something
     // TODO
+    if (currentCashflow == undefined) return;
+    let revenueTotal: number = 0;
+    currentCashflow.getLineItems().forEach((item) => {
+      if (item.getType() == ELineItemType.REVENUE) {
+        revenueTotal += item.getAmount();
+      }
+    });
+
+    let margin: number = input.getVariableAmount();
+    this.calculateExpense(margin, revenueTotal);
   }
   applyTax(input: TaxRate): void {
     this.postTaxAmount = this.amount * input.getResidualMargin();
   }
   getPostTaxAmount(): number {
     return this.postTaxAmount;
+  }
+
+  calculateExpense(margin: number, revenue: number): void {
+    this.amount = margin * revenue;
   }
 }
